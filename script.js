0308e9ff2827a45f9631be35cc6ca614835f97bc
Malaquia24 <arthur.matheus0607@gmail.com>
2023-11-21 15:39:50 -0300

    update

---
+++ script.js
@@ -0,0 +1,64 @@
+const tabela = document.querySelector('.tabela-js')
+const form = document.querySelector('.form-js')
+const BS = document.getElementById('B-S')
+var ID = 0
+
+
+const dados = {}
+
+axios.get('http://127.0.0.1:5000/list').then((response) => {
+    getData(response.data)
+})
+    .catch(function (error) {
+        console.log(error)
+    })
+
+function getData(data) {
+    data.map((item) => {
+        tabela.innerHTML += `
+            <tr>
+                <th scope="row">${item.ID}</th>
+                <td>${item.TAREFA}</td>
+                <td><button class="btn" type="button" data-bs-toggle="modal"
+                data-bs-target="#modalexcluir" onclick="excluir(${item.ID})"><span class="material-symbols-outlined text-danger">
+                delete
+                </span></button>
+                <button class="btn" type="button" data-bs-toggle="modal"
+                data-bs-target="#modaleditar"><span class="material-symbols-outlined text-success">
+                edit
+                </span></button></td>
+            </tr>
+            `
+    })
+}
+
+function Forms(){
+    const tarefa = document.getElementById("Tarefa1").value
+    if (tarefa.trim() !== ""){
+        const json = {Tarefa: tarefa}
+        axios.post('http://127.0.0.1:5000/add', json).then((response) => {
+            console.log("Tarefa adicionada com sucesso", response.Tarefa)
+        })
+        .catch(function (error) {
+            console.error("Ouve um erro na hora de acionar",error)
+        })
+
+    }
+}
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    Forms()
+})
+function excluir(id){
+    ID = id
+}
+BS.addEventListener('click', ()=>{
+    const url = `http://127.0.0.1:5000/delete/${ID}`
+    axios.delete(url).then((response) => {
+        console.log("Excluido com sucesso", response.data)
+    })
+    .catch(function (error) {
+        console.error("Ouve um erro na hora de excluir",error)
+    })
+})
